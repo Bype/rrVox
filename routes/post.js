d00eda5efcecda8a407cbf521703bf4eab1a893f
David Olivari <david.olivari@gmail.com>
2012-12-17 12:35:42 +0100

    missing first character

--- routes/post.js
+++ routes/post.js
@@ -16,8 +16,8 @@ exports.newTxt = function(req, res) {
 		});
 	});
 	if (passed) {
-		req.body.txt = req.body.txt.slice(1, 50)
-		global.red.rpush('msg', req.body.txt.slice(1, 50));
+		req.body.txt = req.body.txt.slice(0, 50)
+		global.red.rpush('msg', req.body.txt.slice(0, 50));
 		global.io.sockets.emit('newtxt', req.body);
 	}
 	res.json(req.body);
